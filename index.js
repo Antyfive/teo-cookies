37505a2e014e4afd632dc763976016a48003eef7
Andrew Teologov <teologov.and@gmail.com>
2016-11-28 23:46:37 +0200

    scaffolding

---
+++ index.js
@@ -0,0 +1,13 @@
+/*!
+ * Teo.JS cookies middleware
+ * @author Andrew Teologov <teologov.and@gmail.com>
+ * @date 11/28/16
+ */
+
+"use strict";
+
+module.exports = {
+    extension(app) {
+
+    }
+};
